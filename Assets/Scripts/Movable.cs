231b9cb77bfb4765dbebcc7aad8bc8239a1c059f
pikachurian <therealpikachurian@gmail.com>
2024-10-21 11:12:16 -0400

    Fixed Sticky Block Bugs

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -7,6 +7,9 @@ using UnityEngine.Windows;
 
 public class Movable : MonoBehaviour
 {
+    [HideInInspector]
+    public bool hasMoved = false;
+
     private GridObject gridObject;
     private Vector2Int nullPosition = new Vector2Int(-1, -1);
 
@@ -46,9 +49,14 @@ public class Movable : MonoBehaviour
 
             if (newPosition != nullPosition)
             {
-                if(transform.tag == "Player")
-                    MoveAdjacentStickyBlocks(gridObject.gridPosition, input);
+                Vector2Int previousGridPosition = gridObject.gridPosition;
                 gridObject.gridPosition = newPosition;
+
+                if (transform.tag == "Player" || transform.tag == "Smooth")
+                {
+                    MoveAdjacentStickyBlocks(previousGridPosition, input);
+                }
+
                 return true;
             }
         }
@@ -59,27 +67,44 @@ public class Movable : MonoBehaviour
     {
         List<Object> stickies = new List<Object>();
         Object obj = null;
-        
+        print(transform.name + " Move Sticky");
         //Up
         obj = GridManager.reference.GetSpaceObject(new Vector2Int(mySpace.x, mySpace.y - 1));
         if (obj != null && obj.GetComponent<Transform>().tag == "Sticky")
+        {
             stickies.Add(obj);
+            //print(obj.name);
+        }
         //Left
         obj = GridManager.reference.GetSpaceObject(new Vector2Int(mySpace.x - 1, mySpace.y));
         if (obj != null && obj.GetComponent<Transform>().tag == "Sticky")
+        {
             stickies.Add(obj);
+            //print(obj.name);
+        }
         //Right
         obj = GridManager.reference.GetSpaceObject(new Vector2Int(mySpace.x + 1, mySpace.y));
         if (obj != null && obj.GetComponent<Transform>().tag == "Sticky")
+        {
             stickies.Add(obj);
+            //print(obj.name);
+        }
         //Down
         obj = GridManager.reference.GetSpaceObject(new Vector2Int(mySpace.x, mySpace.y + 1));
         if (obj != null && obj.GetComponent<Transform>().tag == "Sticky")
-            stickies.Add(obj);
-
-        foreach(Object o in stickies)
         {
+            stickies.Add(obj);
+            //print(obj.name);
+        }
+
+        print(stickies.Count.ToString() + " stickies");
+        foreach (Object o in stickies)
+        {
+            //if (o.GetComponent<Movable>().hasMoved == false)
+            //{
             o.GetComponent<Movable>().Move(input);
+             //   hasMoved = true;
+            //}
         }
 
         print(stickies);
